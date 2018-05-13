8815a67c03a77026b25d7329a55bb43d6f767035
Edward Arcuri <earcuri@paloaltonetworks.com>
2018-05-13 16:23:58 -0600

    cleanup of directories

--- sfn_tools.py
+++ sfn_tools.py
@@ -1,22 +1,35 @@
 import click
+import random
+from lib.utils import *
+from DNS.dns import *
 
 @click.group()
 def cli():
     pass
 
 @click.command()
-@click.option('--file', help='Settings file')
+@click.option('--config', help='Settings file')
 @click.option('--host', help='Host to send generated log messages to', default='localhost')
 @click.option('--port', help='Host port to connect - default is 5514', default=5514)
-def dns(file,host,port):
+def dns(config,host,port):
     click.echo("In DNS")
-    if file:
-        click.echo(f"File is {file}")
+    if config:
+        click.echo(f"File is {config}")
         host="Tester"
-        port=1024        
+        port=1024
+    else:
+        config="DNS/dns.config"
+    click.echo(f"Config is {config}")        
     click.echo(f"Host is {host}")
     click.echo(f"Port is {port}")
 
+    while True:
+        fileNum = random.randint(1,4)
+        fileName = f"lib/log{fileNum}.csv"
+        line = randomLine(open(f"{fileName}"))
+        sendLog('test.sfn',5514,line)
+        time.sleep(1)
+                
 @click.command()
 def iot():
     click.echo("In IoT")
