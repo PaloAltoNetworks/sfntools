a6e6b957133decb6af1bf018476a134a723b9246
Edward Arcuri <earcuri@paloaltonetworks.com>
2018-06-21 13:40:20 -0600

    no idea

--- sfn_tools.py
+++ sfn_tools.py
@@ -1,5 +1,6 @@
 import click
 import random
+import time
 from lib.utils import *
 from DNS.dns import *
 
@@ -23,12 +24,37 @@ def dns(config,host,port):
     click.echo(f"Host is {host}")
     click.echo(f"Port is {port}")
 
-    while True:
-        fileNum = random.randint(1,4)
-        fileName = f"lib/log{fileNum}.csv"
-        line = randomLine(open(f"{fileName}"))
-        sendLog('test.sfn',5514,line)
-        time.sleep(1)
+    flag = True
+    count = 0
+    start = time.time()
+    numEvents = 5000
+    for event in range(numEvents):
+        startDate = "2018/4/19 01:30:00"
+        endDate = "2018/6/15 01:30:00"
+        genDate = randomDate(startDate,endDate,random.random())
+        srcIP = randomLine(open("lib/srcIPs.txt"))
+        srcIP = srcIP.strip()
+        dstIP = randomLine(open("lib/dnsServers.txt"))
+        dstIP = dstIP.strip()
+        threatID = randomLine(open("lib/sigs_list.txt"))
+        threatID = threatID.strip()
+        severity = "informational"
+        msg = f'1,{genDate},015351000011583,THREAT,spyware,2049,{genDate},{srcIP},{dstIP},192.168.55.20,{dstIP},SFN-Logging,,,dns,vsys1,trust,untrust,ethernet1/2,ethernet1/1,SFN-Log-Fowarding,{genDate},18680,1,54848,53,7771,53,0x402000,udp,sinkhole,"",{threatID},any,medium,client-to-server,50115,0x2000000000000000,192.168.0.0-192.168.255.255,United States,0,,0,,,0,,,,,,,,0,12,0,0,0,,ELA-VM-50,,,,,0,,0,,N/A,dns,AppThreat-2606-3102,0x0,0,4294967295'
+        #while flag == True:
+        #fileNum = random.randint(1,4)
+        #fileName = f"lib/log{fileNum}.csv"
+        #fileName = f"lib/logtraffic.csv"
+        #line = randomLine(open(f"{fileName}"))
+        #print(msg)
+    
+        sendLog('test.sfn',5517,msg)
+        count += 1
+        
+    print(f"Started at {start}")
+    print(f"Ended at {time.time()}")
+    print(f"Time to send {count} events: {str(time.time() - start)} seconds")
+    flag = False
+    #time.sleep(1)
                 
 @click.command()
 def iot():
